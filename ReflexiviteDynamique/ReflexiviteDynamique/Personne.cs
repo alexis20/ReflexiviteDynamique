663c4ac0cf3c83ebb4211853306fc4c0685b1dda
qcfrodon <mariobros_ch@hotmail.fr>
2015-05-10 13:24:28 -0400

    #classPersonne

---
+++ ReflexiviteDynamique/ReflexiviteDynamique/Personne.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflexiviteDynamique
+{
+    class Personne
+    {
+        private int Age;
+        private string Nom;
+
+        public Personne()
+        {
+            SetAge(1);
+            SetNom("Bob");
+        }
+
+        public Personne(int a)
+        {
+            SetAge(a);
+            SetNom("George");
+        }
+
+        public Personne(string n)
+        {
+            SetAge(1);
+            SetNom(n);
+        }
+
+        public Personne(int a, string n)
+        {
+            SetAge(a);
+            SetNom(n);
+        }
+
+        public int GetAge()
+        {
+            return Age;
+        }
+
+        public string GetNom()
+        {
+            return Nom;
+        }
+
+        public void SetAge(int a)
+        {
+            Age = a;
+        }
+
+        public void SetNom(string n)
+        {
+            Nom = n;
+        }
+
+        public void AjouterAnnee()
+        {
+            ++Age;
+        }
+
+        public string Saluer()
+        {
+            return "Bonjour " + Nom;
+        }
+    }
+}
