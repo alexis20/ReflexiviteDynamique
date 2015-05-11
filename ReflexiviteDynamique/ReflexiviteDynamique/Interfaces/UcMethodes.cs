aa01609236ee4ea921e884551c47cc1aa9a93d15
qcfrodon <mariobros_ch@hotmail.fr>
2015-05-11 13:57:10 -0400

    fct reusi

--- ReflexiviteDynamique/ReflexiviteDynamique/Interfaces/UcMethodes.cs
+++ ReflexiviteDynamique/ReflexiviteDynamique/Interfaces/UcMethodes.cs
@@ -55,7 +55,7 @@ namespace ReflexiviteDynamique
                 if(reponse != null)
                     MessageBox.Show(reponse.ToString());
                 else
-                    MessageBox.Show("Fct réussis");
+                    MessageBox.Show("Fonction réussis");
             }
             else
             {
@@ -66,7 +66,7 @@ namespace ReflexiviteDynamique
                     if (reponse != null)
                         MessageBox.Show(reponse.ToString());
                     else
-                        MessageBox.Show("Fct réussis");
+                        MessageBox.Show("Fonction réussis");
 
                 }
             }
