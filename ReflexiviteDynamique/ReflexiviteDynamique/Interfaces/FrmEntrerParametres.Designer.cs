eeb0cec0500182da5383c933e4d5835d32625a78
alexis20 <lalonde.alexis@hotmail.com>
2015-05-11 14:07:46 -0400

    dfg

--- ReflexiviteDynamique/ReflexiviteDynamique/Interfaces/FrmEntrerParametres.Designer.cs
+++ ReflexiviteDynamique/ReflexiviteDynamique/Interfaces/FrmEntrerParametres.Designer.cs
@@ -28,90 +28,90 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.components = new System.ComponentModel.Container();
-            this.FlpBottom = new System.Windows.Forms.FlowLayoutPanel();
-            this.BtnAnnuler = new System.Windows.Forms.Button();
-            this.BtnOk = new System.Windows.Forms.Button();
-            this.FlpMain = new System.Windows.Forms.FlowLayoutPanel();
-            this.ErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
-            this.FlpBottom.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // FlpBottom
-            // 
-            this.FlpBottom.Controls.Add(this.BtnAnnuler);
-            this.FlpBottom.Controls.Add(this.BtnOk);
-            this.FlpBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.FlpBottom.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.FlpBottom.Location = new System.Drawing.Point(10, 366);
-            this.FlpBottom.Name = "FlpBottom";
-            this.FlpBottom.Padding = new System.Windows.Forms.Padding(0, 15, 0, 0);
-            this.FlpBottom.Size = new System.Drawing.Size(528, 63);
-            this.FlpBottom.TabIndex = 1;
-            // 
-            // BtnAnnuler
-            // 
-            this.BtnAnnuler.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.BtnAnnuler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnAnnuler.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnAnnuler.Location = new System.Drawing.Point(450, 18);
-            this.BtnAnnuler.Name = "BtnAnnuler";
-            this.BtnAnnuler.Size = new System.Drawing.Size(75, 23);
-            this.BtnAnnuler.TabIndex = 0;
-            this.BtnAnnuler.Text = "Annuler";
-            this.BtnAnnuler.UseVisualStyleBackColor = true;
-            // 
-            // BtnOk
-            // 
-            this.BtnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnOk.Location = new System.Drawing.Point(369, 18);
-            this.BtnOk.Name = "BtnOk";
-            this.BtnOk.Size = new System.Drawing.Size(75, 23);
-            this.BtnOk.TabIndex = 1;
-            this.BtnOk.Text = "Ok";
-            this.BtnOk.UseVisualStyleBackColor = true;
-            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
-            // 
-            // FlpMain
-            // 
-            this.FlpMain.AutoSize = true;
-            this.FlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.FlpMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.FlpMain.Location = new System.Drawing.Point(10, 10);
-            this.FlpMain.Name = "FlpMain";
-            this.FlpMain.Size = new System.Drawing.Size(528, 356);
-            this.FlpMain.TabIndex = 2;
-            // 
-            // ErrorProvider
-            // 
-            this.ErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
-            this.ErrorProvider.ContainerControl = this;
-            // 
-            // FrmEntrerParametres
-            // 
-            this.AcceptButton = this.BtnOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.CancelButton = this.BtnAnnuler;
-            this.ClientSize = new System.Drawing.Size(548, 439);
-            this.Controls.Add(this.FlpMain);
-            this.Controls.Add(this.FlpBottom);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(200, 150);
-            this.Name = "FrmEntrerParametres";
-            this.Padding = new System.Windows.Forms.Padding(10);
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Entrer les paramètres";
-            this.FlpBottom.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			this.FlpBottom = new System.Windows.Forms.FlowLayoutPanel();
+			this.BtnAnnuler = new System.Windows.Forms.Button();
+			this.BtnOk = new System.Windows.Forms.Button();
+			this.FlpMain = new System.Windows.Forms.FlowLayoutPanel();
+			this.ErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+			this.FlpBottom.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// FlpBottom
+			// 
+			this.FlpBottom.Controls.Add(this.BtnAnnuler);
+			this.FlpBottom.Controls.Add(this.BtnOk);
+			this.FlpBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.FlpBottom.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+			this.FlpBottom.Location = new System.Drawing.Point(10, 391);
+			this.FlpBottom.Name = "FlpBottom";
+			this.FlpBottom.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+			this.FlpBottom.Size = new System.Drawing.Size(528, 38);
+			this.FlpBottom.TabIndex = 1;
+			// 
+			// BtnAnnuler
+			// 
+			this.BtnAnnuler.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.BtnAnnuler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnAnnuler.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BtnAnnuler.Location = new System.Drawing.Point(450, 13);
+			this.BtnAnnuler.Name = "BtnAnnuler";
+			this.BtnAnnuler.Size = new System.Drawing.Size(75, 23);
+			this.BtnAnnuler.TabIndex = 0;
+			this.BtnAnnuler.Text = "Annuler";
+			this.BtnAnnuler.UseVisualStyleBackColor = true;
+			// 
+			// BtnOk
+			// 
+			this.BtnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BtnOk.Location = new System.Drawing.Point(369, 13);
+			this.BtnOk.Name = "BtnOk";
+			this.BtnOk.Size = new System.Drawing.Size(75, 23);
+			this.BtnOk.TabIndex = 1;
+			this.BtnOk.Text = "Ok";
+			this.BtnOk.UseVisualStyleBackColor = true;
+			this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+			// 
+			// FlpMain
+			// 
+			this.FlpMain.AutoSize = true;
+			this.FlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.FlpMain.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+			this.FlpMain.Location = new System.Drawing.Point(10, 10);
+			this.FlpMain.Name = "FlpMain";
+			this.FlpMain.Size = new System.Drawing.Size(528, 381);
+			this.FlpMain.TabIndex = 2;
+			// 
+			// ErrorProvider
+			// 
+			this.ErrorProvider.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+			this.ErrorProvider.ContainerControl = this;
+			// 
+			// FrmEntrerParametres
+			// 
+			this.AcceptButton = this.BtnOk;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.CancelButton = this.BtnAnnuler;
+			this.ClientSize = new System.Drawing.Size(548, 439);
+			this.Controls.Add(this.FlpMain);
+			this.Controls.Add(this.FlpBottom);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(200, 150);
+			this.Name = "FrmEntrerParametres";
+			this.Padding = new System.Windows.Forms.Padding(10);
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Entrer les paramètres";
+			this.FlpBottom.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
